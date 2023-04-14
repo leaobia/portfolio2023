b35d87023cc7054872566067b208f681fe94160d
unknown <biancapeleao@gmail.com>
2023-04-14 10:17:27 -0300

    projetos iniciado

--- js/header.js
+++ js/header.js
@@ -2,12 +2,23 @@
 
 const menu = document.querySelector(".menu-burger-container")
 const li = document.querySelector(".links-container")
+const links = document.querySelectorAll(".link")
 
 const active = () => {
     menu.classList.toggle('active')
     li.classList.toggle('d-none')
     li.classList.toggle('ativo')
 }
+const clicadoLink = () => {
+
+    menu.classList.toggle('active')
+    li.classList.toggle('d-none')
+    li.classList.toggle('ativo')
+
+}
 
 menu.addEventListener('click', active)
+links.forEach(link2 => {
+    link2.addEventListener('click', clicadoLink)
+})
 
